b11c36d3a94faa0817810dd6fbf9a749c95afccb
badjou2002 <bil602u@gmail.com>
2025-04-13 15:06:09 +0100

    mon premier commit

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,10 +1,21 @@
-var admin = require("firebase-admin");
-
-var serviceAccount = require("./jcivoting-firebase-adminsdk-fbsvc-c723c00d40.json");
+require("dotenv").config();
+const admin = require("firebase-admin");
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://jcivoting-default-rtdb.firebaseio.com"
+  credential: admin.credential.cert({
+    type: "service_account",
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKeyId: process.env.FIREBASE_PRIVATE_KEY_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    clientId: process.env.FIREBASE_CLIENT_ID,
+    authUri: process.env.FIREBASE_AUTH_URI,
+    tokenUri: process.env.FIREBASE_TOKEN_URI,
+    authProviderX509CertUrl: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+    clientC509CertUrl: process.env.FIREBASE_CLIENT_CERT_URL,
+    universeDomain: process.env.FIREBASE_UNIVERSE_DOMAIN,
+  }),
+  databaseURL: "https://jcivoting-default-rtdb.firebaseio.com"
 });
 
 const db = admin.database();
